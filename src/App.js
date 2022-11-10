611b7fda6b63b4255d476ac31b69ac3c057861a8
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-10 16:23:38 +0200

    key prop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from "react";
 import Greeting from './components/Greeting';
-
-const userArray = ['John', 'Alex', 'Jake', 'Jane', 'Johs'];
+import {userData} from "./components/Greeting/userData";
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: userData,
+            isSort: true
+        }
+    }
  
-    userMap = () => userArray.map((name) => <Greeting userName={name} />)
+    userMap = () => this.state.users.map((userObj) => <Greeting userName={userObj.name} key={userObj.id}/>);
+
+    sortUsers = () => {
+        const {users, isSort} = this.state;
+        const newUsers = [...users];
+        newUsers.sort((a,b) => (a.name > b.name && isSort) ? 1 : -1);
+        this.setState({
+            users: newUsers,
+            isSort: !isSort
+        })
+    }
 
     render () {
         return (
             <section>
+                <button onClick={this.sortUsers}>Sorted</button>
                 {this.userMap()}
             </section>
         )
