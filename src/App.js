a174e3850f20e317571bd1176193aa2c058583ed
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-11 15:06:12 +0200

    Lifecycle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import UserDashboard from "./components/UserDashboard";
+import Tree from './components/Tree';
 
 class App extends React.Component {
-
- 
-
+    constructor(props){
+        super(props);
+        this.state = {
+            isOn: true
+        }
+    }
 
     render () {
         return (
-            <UserDashboard />  
+            <div>
+                <button onClick={()=>{this.setState({isOn: false})}}>Unmount Tree</button>
+                 {this.state.isOn ? <Tree /> : null} 
+           </div>
         )
     }
 }
