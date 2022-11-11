07e529e547755cc03cb324dee1dce33e4f1359b4
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-11 16:09:34 +0200

    date-fns timer

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
+import {format, addSeconds} from 'date-fns';
 
 class Timer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 0
+            time: new Date(0,0,0,0,0,0)
         }
         this.intervalId = null;
     }
 
     start = () => {
        this.intervalId = setInterval(()=>{
-            const {count} = this.state;
+            const {time} = this.state;
+        const newdate = addSeconds(time, 1);
             this.setState({
-                count: count+1
+                time: newdate
             })
+            
         }, 1000);
-       
     }
     componentDidMount(){
-        this.start();
+ //       this.start();
     }
 
     componentWillUnmount(){
@@ -31,10 +33,12 @@ class Timer extends Component {
     }
 
     render() {
+        const {time} = this.state;
         return (
             <div>
-               <h1>{this.state.count}</h1> 
+               <h1>{format(time, 'hh:mm:ss')}</h1> 
                <button onClick={this.stop}>Stop</button>
+               <button onClick={this.start}>Start</button>
             </div>
         );
     }
