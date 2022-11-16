3d8519e6b812c9f513ccf4b38bbefe310b6c4d46
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-16 14:29:14 +0200

    Todolist task resolve v.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import LoginForm from './components/LoginForm'
+import TodoList from './components/TodoList';
 
 class App extends React.Component {
     constructor(props){
@@ -11,7 +11,7 @@ class App extends React.Component {
 
     render () {
         return (
-           <LoginForm />
+           <TodoList />
         )
     }
 }
---
+++ src/components/TodoList/TodoItem/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TodoItem = (props) => {
+    return (
+        <li>{props.text}</li>
+    );
+}
+
+export default TodoItem;
