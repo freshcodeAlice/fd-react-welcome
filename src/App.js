25fb6590d37104fade566c3b3352dd64742ecf28
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-17 15:48:40 +0200

    Custom grid

--- src/App.js
+++ src/App.js
@@ -1,41 +1,49 @@
 import React from "react";
+import Column from "./components/Grid/Column";
+import Container from "./components/Grid/GridContainer";
+import Row from "./components/Grid/Row";
 import Header from "./components/Header";
 
 class App extends React.Component {
     constructor(props){
         super(props);
-        this.state = {
-            isOn: true,
-            user: {
-                firstName: 'Alex',
-                lastName: 'Doe',
-                avatar: 'https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg'
-            }
-        }
     }
 
     render () {
-        const {user: {firstName, lastName, avatar}} = this.state;
+
         return (
-           <Header text="value">
-               <div>
-                <img src={avatar} width="75px" height="75px"/>
-                <span>{firstName} {lastName} </span>
-                </div>
-            </Header>
+            <Container>
+                <Row>
+                    <Column colNum="12">
+                        Full-width element
+                    </Column>
+                </Row>
+                <Row>
+                    <Column colNum="6">
+                    Half-width
+                    </Column>
+                    <Column colNum="6">
+                    Half-width
+                    </Column>
+                </Row>
+                <Row>
+                    <Column colNum="3">
+                    Quart-width
+                    </Column>
+                    <Column colNum="3">
+                    Half-width
+                    </Column>
+                    <Column colNum="3">
+                    Quart-width
+                    </Column>
+                    <Column colNum="3">
+                    Half-width
+                    </Column>
+                </Row>
+            </Container>
         )
     }
 }
 
 export default App;
 
-
-/*
-Список справ (TodoList)
-
-1. Форма (інпут і кнопка), за введення даних і натиснення кнопки вміст інпута стає новим рядком у списку справ
-2. Список справ відображається як ul-список. Кожна справа - це лішка.
-
-Дві компоненти - одна - список і друга - елемент списку
-
-*/
