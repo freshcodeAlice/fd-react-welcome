4179c1797fe521f3d687d20327740eac7cbcf642
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-17 16:57:24 +0200

    FlexContainer task resolve

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import FlexContainer from "./components/FlexContainer";
 import Column from "./components/Grid/Column";
 import Container from "./components/Grid/GridContainer";
 import Row from "./components/Grid/Row";
 import Header from "./components/Header";
+import styles from './App.module.css';
 
 class App extends React.Component {
     constructor(props){
@@ -12,38 +14,26 @@ class App extends React.Component {
     render () {
 
         return (
-            <Container>
-                <Row>
-                    <Column colNum="12">
-                        Full-width element
-                    </Column>
-                </Row>
-                <Row>
-                    <Column colNum="6">
-                    Half-width
-                    </Column>
-                    <Column colNum="6">
-                    Half-width
-                    </Column>
-                </Row>
-                <Row>
-                    <Column colNum="3">
-                    Quart-width
-                    </Column>
-                    <Column colNum="3">
-                    Half-width
-                    </Column>
-                    <Column colNum="3">
-                    Quart-width
-                    </Column>
-                    <Column colNum="3">
-                    Half-width
-                    </Column>
-                </Row>
-            </Container>
+         <FlexContainer fd="row" jc="space-between" ai="center">
+            <div className={styles.box}> 1 </div>
+            <div className={styles.box}> 2 </div>
+            <div className={styles.box}> 3 </div>
+            <div className={styles.box}> 4 </div>
+            <div className={styles.box}> 5 </div>
+         </FlexContainer>
         )
     }
 }
 
 export default App;
 
+
+/*
+
+Зробити компонент ФлексКонтейнер, який буде відображати будь-яку кількість div-дітей.
+Він відображає дітей як флекс-дітей. 
+Флекс-орієнтація, розташування дітей за головною і побічною віссю - це налаштування компоненти
+
+
+
+*/
