c511a2be7bc3c6a9b61feae758c8c0fc8e878f34
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-18 16:08:51 +0200

    request multiple users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ImageWrapper from "./components/ImageWrapper";
+import UserDashboard from "./components/UserDashboard";
 
 class App extends React.Component {
     constructor(props){
@@ -9,35 +9,10 @@ class App extends React.Component {
     render () {
 
         return (
-            <>
-        <ImageWrapper
-            width="200px"
-            height="300px"
-            onClick={()=>{console.log('hello')}}
-        >
-            <img src='https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg' width="100%"/>
-        </ImageWrapper>
-                <ImageWrapper
-                width="200px"
-                height="300px"
-                title="My super picture"
-            >
-                <img src='https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg' width="100%"/>
-            </ImageWrapper>
-            </>
+         <UserDashboard />
         )
     }
 }
 
 export default App;
 
-
-/*
-
-Зробити компонент ФлексКонтейнер, який буде відображати будь-яку кількість div-дітей.
-Він відображає дітей як флекс-дітей. 
-Флекс-орієнтація, розташування дітей за головною і побічною віссю - це налаштування компоненти
-
-
-
-*/
