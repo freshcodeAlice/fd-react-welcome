46bd6702e30833ca09e5dd1a0739327e6d6b8493
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-21 15:42:08 +0200

    GetProducts task resolve

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ProductsList from "./components/ProductsList ";
 import UserDashboard from "./components/UserDashboard";
 
 class App extends React.Component {
@@ -9,7 +10,7 @@ class App extends React.Component {
     render () {
 
         return (
-         <UserDashboard />
+        <ProductsList />
         )
     }
 }
