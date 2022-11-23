143f536a62fcf0beedea53dc43f9d0d81d6f9e6b
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-23 14:57:58 +0200

    shouldComponentUpdate

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import Counter from './components/Counter';
 import LoginForm from './components/LoginForm';
 import TodoList from './components/TodoList';
+import CounterPage from './pages/CounterPage';
 
 
 class App extends React.Component {
@@ -16,11 +17,13 @@ class App extends React.Component {
             <BrowserRouter>
                 <ul>
                     <li><Link to="/">Go to Home</Link></li>
+                    <li><Link to="/counter">Go to CounterPage</Link></li>
                     <li><Link to="/login">Go to LoginForm</Link></li>
                     <li><Link to="/todo">Go to Todo</Link></li>
                 </ul>
                 <Routes>
                     <Route path="/" element={<Home />}/>
+                    <Route path="/counter" element={<CounterPage />}/>
                     <Route path="/login" element={<LoginForm />}/>
                     <Route path="/todo" element={<TodoList />}/>
                     <Route path="*" element={<NotFound />}/>
