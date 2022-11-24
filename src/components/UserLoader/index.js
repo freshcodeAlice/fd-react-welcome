f86535722ed03f90d44741fc60b86ea88e2b710c
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-24 16:04:23 +0200

    Render-props

---
+++ src/components/UserLoader/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class UserLoader extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            isFetching: true,
+            error: null
+        }
+    }
+
+    componentDidMount() {
+        this.props.loadData()
+        .then((data) => {
+            this.setState({
+                data
+            })
+        })
+        .catch((error)=>{
+            this.setState({
+                error
+            })
+        })
+        .finally(()=>{
+            this.setState({
+                isFetching: false
+            })
+        })
+    }
+    
+    render() {
+      const layout = this.props.renderThis(this.state);
+      return layout;
+    }
+}
+
+export default UserLoader;
