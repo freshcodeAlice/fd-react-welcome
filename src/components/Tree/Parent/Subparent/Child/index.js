0802de04f9947a06404c6d51c8b32e54a60208eb
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-28 15:17:40 +0200

    Context

---
+++ src/components/Tree/Parent/Subparent/Child/index.js
@@ -0,0 +1,13 @@
+import Innerchild from './Innerchild';
+
+function Child (props) {
+    return (
+      <div style={{border: '2px solid black', padding: '20px'}}>
+        <p>Child</p>
+          <Innerchild/>
+       </div>
+    )
+ }
+ 
+ export default Child;
+ 
