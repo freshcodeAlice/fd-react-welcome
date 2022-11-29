b2bdc6b8c8faed34c7c1a6c598e747cb2c29aac0
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-29 15:12:35 +0200

    HOC

---
+++ src/HOCs/withUser.js
@@ -0,0 +1,10 @@
+import UserContext from "../contexts/UserContext";
+
+
+export const withUser = (Component) => (props) =>  (
+    <UserContext.Consumer>
+        {([user, setUser])=>{
+          return <Component user={user} setUser={setUser}/>
+          }}
+   </UserContext.Consumer>
+  );
--- src/components/Tree/index.js
+++ src/components/Tree/index.js
@@ -5,25 +5,32 @@ import cx from 'classnames';
 import ThemeContext from '../../contexts/ThemeContext';
 import CONSTANTS from '../../constants';
 const {THEMES} = CONSTANTS;
+import UserContext from '../../contexts/UserContext';
 
 function Tree (props) {
-
-  return (
-   <ThemeContext.Consumer>
-      {([theme, setTheme])=>{
-          const cn = cx(styles.container, {
-            [styles.darkTheme]: theme === THEMES.DARK,
-            [styles.lightTheme]: theme === THEMES.LIGHT
-          });
-          return (
-             <div className={cn}>
-                <p>Tree</p>
-                <Parent />
-             </div>
-          )
-      }}
-   </ThemeContext.Consumer>
-  )
+   return (
+      <UserContext.Consumer>
+         {([user, setUser])=>{
+      return (
+         <ThemeContext.Consumer>
+            {([theme, setTheme])=>{
+               const cn = cx(styles.container, {
+                  [styles.darkTheme]: theme === THEMES.DARK,
+                  [styles.lightTheme]: theme === THEMES.LIGHT
+               });
+               return (
+                  <div className={cn}>
+                     <p>{user.firstName}</p>
+                     <p>Tree</p>
+                     <Parent />
+                  </div>
+               )
+            }}
+         </ThemeContext.Consumer>
+      )
+         }}
+      </UserContext.Consumer>
+   )
 }
 
 export default Tree;
