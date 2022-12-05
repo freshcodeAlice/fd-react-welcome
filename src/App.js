6a588e8b5077526c2c077b90f11428aadb3c14d5
freshcodeAlice <freshjs.ofte@gmail.com>
2022-12-05 15:20:41 +0200

    this.setState and useState difference

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React, {useState} from "react";
-
+import MouseTracker from "./components/MouseTracker";
 
 function App(props) {
-     const [count, setCount] = useState(0);
-
-    const increment = () => {
-       setCount(count+1);
-    }
+   
 
     return (
-        <>
-        <h2>{count}</h2>
-        <button onClick={increment}>+</button>
-        </>
+       <MouseTracker />
     )
 }
 
